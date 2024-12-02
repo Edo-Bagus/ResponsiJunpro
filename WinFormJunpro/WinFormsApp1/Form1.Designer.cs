014688055a8b95ce74fd56b9320420305ce969f9
Edo Bagus <ebagus409@gmail.com>
2024-12-02 09:13:52 +0700

    Responsi Junpro 2

---
+++ WinFormJunpro/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNama = new TextBox();
+            dataGridView1 = new DataGridView();
+            comboDep = new ComboBox();
+            comboJabatan = new ComboBox();
+            btnInsert = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            listBox1 = new ListBox();
+            label4 = new Label();
+            label5 = new Label();
+            listBox2 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nama:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Dep. Karyawan";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Jabatan";
+            // 
+            // txtNama
+            // 
+            txtNama.Location = new Point(115, 25);
+            txtNama.Name = "txtNama";
+            txtNama.Size = new Size(121, 23);
+            txtNama.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 160);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(389, 150);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellClick += dataGridView1_CellContentClick;
+            // 
+            // comboDep
+            // 
+            comboDep.FormattingEnabled = true;
+            comboDep.Location = new Point(115, 53);
+            comboDep.Name = "comboDep";
+            comboDep.Size = new Size(121, 23);
+            comboDep.TabIndex = 5;
+            // 
+            // comboJabatan
+            // 
+            comboJabatan.FormattingEnabled = true;
+            comboJabatan.Location = new Point(115, 82);
+            comboJabatan.Name = "comboJabatan";
+            comboJabatan.Size = new Size(121, 23);
+            comboJabatan.TabIndex = 6;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(22, 121);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(75, 23);
+            btnInsert.TabIndex = 7;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(115, 121);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(75, 23);
+            btnEdit.TabIndex = 8;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(220, 121);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(406, 41);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(406, 23);
+            label4.Name = "label4";
+            label4.Size = new Size(93, 15);
+            label4.TabIndex = 11;
+            label4.Text = "ID DEPARTEMEN";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(407, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(61, 15);
+            label5.TabIndex = 12;
+            label5.Text = "ID Jabatan";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(407, 178);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(292, 94);
+            listBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 347);
+            Controls.Add(listBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(listBox1);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnInsert);
+            Controls.Add(comboJabatan);
+            Controls.Add(comboDep);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtNama);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNama;
+        private DataGridView dataGridView1;
+        private ComboBox comboDep;
+        private ComboBox comboJabatan;
+        private Button btnInsert;
+        private Button btnEdit;
+        private Button btnDelete;
+        private ListBox listBox1;
+        private Label label4;
+        private Label label5;
+        private ListBox listBox2;
+    }
+}
---
+++ WinFormJunpro/WinFormsApp1/Form1.cs
@@ -0,0 +1,204 @@
+using Npgsql;
+using System.Data;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connString = "Host=localhost;Port=5432;Username=postgres;Password=informatika;Database=ResponsiJunpro";
+        NpgsqlConnection conn;
+        NpgsqlCommand cmd;
+        DataTable dt;
+        DataGridViewRow r;
+        string sql = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection(connString);
+            LoadDepartemen();
+            LoadKaryawan();
+            LoadJabatan();
+        }
+
+        private void LoadDepartemen()
+        {
+            try
+            {
+                sql = "SELECT * FROM departemen";
+                conn.Open();
+                comboDep.DataSource = null;
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                conn.Close();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    comboDep.Items.Add(dr[0]);
+                    string listStr = dr[0].ToString() + ": " + dr[1].ToString();
+                    listBox1.Items.Add(listStr);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void LoadJabatan()
+        {
+            try
+            {
+                sql = "SELECT * FROM jabatan";
+                conn.Open();
+                comboJabatan.DataSource = null;
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                conn.Close();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    comboJabatan.Items.Add(dr[0]);
+                    string listStr = dr[0].ToString() + ": " + dr[1].ToString()+ ", salary: " + dr[2].ToString();
+                    listBox2.Items.Add(listStr);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void LoadKaryawan()
+        {
+            try
+            {
+                sql = "SELECT * FROM karyawan";
+                conn.Open();
+                dataGridView1.DataSource = null;
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                conn.Close();
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Debug.WriteLine("SETSGE");
+            Debug.WriteLine(e.RowIndex);
+            if (e.RowIndex >= 0)
+            {
+                r = dataGridView1.Rows[e.RowIndex];
+                txtNama.Text = r.Cells["nama"].Value.ToString();
+                comboDep.SelectedItem = r.Cells["id_dep"].Value;
+                comboJabatan.SelectedItem = r.Cells["id_jabatan"].Value;
+
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * FROM insert_karyawan(:_name,:_id_dep,:_id_jabatan)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_name", txtNama.Text);
+                cmd.Parameters.AddWithValue("_id_dep", comboDep.SelectedItem);
+                cmd.Parameters.AddWithValue("_id_jabatan", comboJabatan.SelectedItem);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data Users Berhasil diinputkan", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    LoadKaryawan();
+                    txtNama.Text = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Insert FAIL !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon Pilih Baris yang akan dihapus", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (MessageBox.Show("Apakah benar anda ingin menghapus data " + r.Cells["nama"].Value.ToString() + " ?",
+                "Hapus data terkonfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"DELETE FROM karyawan WHERE id_karyawan = :_id_karyawan";
+                    Debug.WriteLine(r.Cells["id_karyawan"].Value.ToString());
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id_karyawan", r.Cells["id_karyawan"].Value.ToString());
+                    cmd.ExecuteScalar();
+                    MessageBox.Show("Data Users Berhasil dihapus", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    LoadKaryawan();
+                    txtNama.Text = null;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:" + ex.Message, "Delete FAIL !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon Pilih Baris yang akan diupdate", "Good!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * FROM update_karyawan(:_id,:_nama,:_id_dep,:_id_jabatan)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", r.Cells["id_karyawan"].Value.ToString());
+                cmd.Parameters.AddWithValue("_nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("_id_dep", comboDep.SelectedItem);
+                cmd.Parameters.AddWithValue("_id_jabatan", comboJabatan.SelectedItem);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    conn.Close();
+                    MessageBox.Show("Data Users Berhasil diupdate", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadKaryawan();
+                    txtNama.Text = null;
+                    r = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Insert FAIL !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
